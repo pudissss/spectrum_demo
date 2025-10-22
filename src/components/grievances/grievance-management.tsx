c7238db8cf8c79c354b9491476deb8f7df9a3397
Pudi Shanmukha Sri Sai <shanu.pudi@gmail.com>
2025-10-22 12:51:26 +0000

    Try fixing this error: `Runtime Error: Error: DropdownMenuSeparator is n

--- src/components/grievances/grievance-management.tsx
+++ src/components/grievances/grievance-management.tsx
@@ -9,6 +9,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
